8cf39e3b6b12e0d7516cdde358ffc0fb5b6d9314
lfonta2001 <lfontaine2001@gmail.com>
2025-04-11 10:08:33 -0300

    Future gratest page

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('root')).render(
+    <div class="flex flex-col w-full h-full p-4 place-items-center">
+      <h1 class="text-4xl font-bold underline">Proximamente</h1>
+    </div>
+)
